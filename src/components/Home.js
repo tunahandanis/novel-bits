54ad1fcf17728ec0527f9d55fac63deb379ffc4b
Tunahan Danış <90687687+tunahandanis@users.noreply.github.com>
2023-01-16 21:38:18 +0300

    cleanup

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import Head from "next/head"
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Novelbits</title>
+        <meta name="description" content="Novelbits Landing Page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main></main>
+    </>
+  )
+}
+
+export default Home

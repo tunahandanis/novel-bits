3e50c410ed1d2a651d25165451df414de4c789c2
Tunahan Danış <90687687+tunahandanis@users.noreply.github.com>
2023-03-04 16:05:28 +0300

    add hardhat

---
+++ web3/hardhat.config.js
@@ -0,0 +1,20 @@
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    testnet: {
+      url: `https://rpc.testnet.fantom.network`,
+      chainId: 4002,
+      accounts: [`${process.env.PRIVATE_KEY}`],
+    },
+    coverage: {
+      url: "http://localhost:8555",
+    },
+    localhost: {
+      url: `http://127.0.0.1:8545`,
+    },
+  },
+}

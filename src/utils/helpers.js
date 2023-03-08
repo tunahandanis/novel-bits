a5b66446cdd4ad73a17487b3274cfb4294fd046e
Tunahan Danış <90687687+tunahandanis@users.noreply.github.com>
2023-03-09 00:08:15 +0300

    add author to columns

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,7 @@
+import { message } from "antd"
 import Link from "next/link"
+import CopyToClipboard from "react-copy-to-clipboard"
+import { formatAccount } from "./common"
 
 export const getBooksTableColumns = (isAuthor) => {
   const columns = {
@@ -18,6 +21,26 @@ export const getBooksTableColumns = (isAuthor) => {
       key: "chapters",
       dataIndex: "",
       render: (row) => row.chapters.length,
+    },
+    Author: {
+      title: () => <strong>Author</strong>,
+      key: "author",
+      dataIndex: "authorWalletAddress",
+      render: (add) => (
+        <CopyToClipboard
+          text={add}
+          onCopy={() => {
+            message.open({
+              type: "info",
+              content: "Copied to clipboard",
+            })
+          }}
+        >
+          <span style={{ color: "#40a9ff", cursor: "pointer" }}>
+            {formatAccount(add)}
+          </span>
+        </CopyToClipboard>
+      ),
     },
     Link: {
       title: () => <strong>Go To Book</strong>,
